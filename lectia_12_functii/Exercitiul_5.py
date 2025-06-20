79d329c2ea7dd9a492b31293d02b1a029af56982
Siminiuc <siminiuc.rodica@gmail.com>
2025-06-20 16:15:51 +0200

    Exercitiul_5_par-impar

--- lectia_12_functii/Exercitiul_5.py
+++ lectia_12_functii/Exercitiul_5.py
@@ -7,8 +7,18 @@ Exemplu: Pentru n=10 rezultatul returnat va fi ["impar", "par", "impar", "par", 
 # CODUL TĂU VINE MAI JOS:
 
 def task_5(n):
+    for x in range(1, n + 1):
+      if x % 2 == 0: 
+        return "par"
+    else: 
+        return "impar" 
+
+print(task_5(15))  # De ce in cazul meu da rezultat doar o data par, desi eu am specificat (15)
+
+# CODUL TĂU VINE MAI SUS:
+
+"""Cu ajutorul GPT"""
+def task_5(n):
     return ["par" if x % 2 == 0 else "impar" for x in range(1, n + 1)]
 
 print(task_5(15))
-
-# CODUL TĂU VINE MAI SUS:
